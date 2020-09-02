d203ee4c7d3c77c3c185023147a4021d39ec9a32
Sarina Burch <sarinaburch52@gmail.com>
2020-09-01 20:38:58 -0400

    added all get routes

--- server.js
+++ server.js
@@ -2,23 +2,34 @@
 var express = require("express");
 var app = express();
 var PORT = 3000;
+var dbJson = require("./db/db.json");
+var path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
 
 var notes = "hey"
+
 //routes
 app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./notes.html"));
 });
 app.get("/api/notes", function(req, res) {
-    return res.json(notes);
-});
-app.post("/api/notes:id", function(req, res) {
-    console.log(req.body)
+    res.json(dbJson)
+})
+app.post("/notes:id", function(req, res) {
+    var notes = req.body;
 });
 app.delete("/api/notes", function(req, res) {
     res.sendFile
 });
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "./index.html"))
 });
 //Listener
 app.listen(PORT, function() {
